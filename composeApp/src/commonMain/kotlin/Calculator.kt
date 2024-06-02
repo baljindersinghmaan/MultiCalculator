6fb0ff9e0898cef0caf1e2518d2b5ac1f80c1dd4
baljinder <baljindermaan15@gmail.com>
2024-06-02 15:24:47 -0400

    add, divide, multiply and subtract methods are added

---
+++ composeApp/src/commonMain/kotlin/Calculator.kt
@@ -0,0 +1,15 @@
+class Calculator {
+    fun add(left: Double, right: Double): Double {
+        return left.plus(right)
+    }
+    fun subtract(left: Double, right: Double): Double {
+        return left.minus(right)
+    }
+    fun divide(left: Double, right: Double): Double {
+        return left.div(right)
+    }
+    fun multiply(left: Double, right: Double): Double {
+        return left * right
+    }
+
+}
