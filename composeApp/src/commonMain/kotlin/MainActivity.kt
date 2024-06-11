bf2d79c9251ebb12fd0a434e43e0f1777407d068
baljinder <baljindermaan15@gmail.com>
2024-06-10 22:11:09 -0400

    CalcView Created

--- composeApp/src/commonMain/kotlin/MainActivity.kt
+++ composeApp/src/commonMain/kotlin/MainActivity.kt
@@ -1,7 +1,11 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Colors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -10,17 +14,32 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class MainActivity {
 
     @Composable
     fun CalcView(){
+        val displayText = mutableStateOf("0")
+        Column(modifier = Modifier.background(Color.LightGray)) {
+            Row { CalcView() }
+            Row {
+                Column {  }
+                Column {  }
+            }
+        }
 
     }
 
     @Composable
-    fun CalcRow(){
+    fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int){
+        val endNum = startNum + numButtons
+        Row(modifier = Modifier.padding(0.dp)) {
+            for (i in startNum..<endNum) {
+                CalcNumericButton(i, mutableStateOf(i.toString()))
+            }
+        }
 
     }
 
@@ -34,7 +53,7 @@ class MainActivity {
             onClick = { display.value += number } ,
             modifier = Modifier.padding(4.dp)
         ) {
-            Text(text = display.value)
+            Text(text = number.toString())
         }
     }
 
